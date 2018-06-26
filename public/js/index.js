d008b3a085a6f62eb9dc406da6512ddedf6c60ef
unknown <Poojagirisha>
2018-06-26 13:08:41 +0530

    location

--- public/js/index.js
+++ public/js/index.js
@@ -22,22 +22,42 @@ var socket=io();
     // });
 
     socket.on('newMessage',function(message){
-        //console.log('newMessage',message);
         var formattedTime=moment(message.createdAt).format('h:mm a');
-        var li =jQuery('<li></li>');
-        li.text(`${message.from} ${formattedTime} :${message.text}`);
+        var template=jQuery('#message-template').html();
+        var html= Mustache.render(template,{
+            //text: (`${message.from} ${formattedTime} :${message.text}`)
+            text: message.text,
+            from: message.from,
+            createdAt: formattedTime
+        });
 
-        jQuery('#messages').append(li);
+        jQuery('#messages').append(html);
+        //console.log('newMessage',message);
+        // var formattedTime=moment(message.createdAt).format('h:mm a');
+        // var li =jQuery('<li></li>');
+        // li.text(`${message.from} ${formattedTime} :${message.text}`);
+
+        // jQuery('#messages').append(li);
     });
 
     socket.on('newLocationMessage', function(message){
-        var li=jQuery('<li></li>');
+        // var li=jQuery('<li></li>');
         var formattedTime=moment(message.createdAt).format('h:mm a');
-        var a=jQuery('<a target="_blank"> my current location</a>');
-        li.text(`${message.from} ${formattedTime}:`);
-        a.attr('href',message.url);
-        li.append(a);
-        jQuery('#messages').append(li);
+        var template=jQuery('#location-message-template').html();
+        var html= Mustache.render(template,{
+            //text: (`${message.from} ${formattedTime} :${message.text}`)
+            from: message.from,
+            url: message.url,
+            createdAt: formattedTime
+        });
+
+        jQuery('#messages').append(html);
+
+        // var a=jQuery('<a target="_blank"> my current location</a>');
+        // li.text(`${message.from} ${formattedTime}:`);
+        // a.attr('href',message.url);
+        // li.append(a);
+        // jQuery('#messages').append(li);
     });
 
     // socket.emit('createMessage',{

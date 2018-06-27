10700a5556ad67360d9a271b8195331b1175d8ef
unknown <Poojagirisha>
2018-06-27 12:40:56 +0530

    ststusx

--- public/js/chat.js
+++ public/js/chat.js
@@ -113,7 +113,6 @@ socket.on('updateUserList',function(users){
         var messageTextbox=jQuery('[name=message]');
 
         socket.emit('createMessage',{
-            from:'User',
             text:messageTextbox.val()
         },function(){
             messageTextbox.val('')
--- server/server.js
+++ server/server.js
@@ -55,8 +55,14 @@ callback();
 // });
 
 socket.on('createMessage',(message,callback)=>{
-    console.log('create message',message);
-    io.emit('newMessage',generateMessage(message.from,message.text));
+    //console.log('create message',message);
+    var user=users.getUser(socket.id);
+if(user && isRealString(message.text)){
+
+    io.to(user.room).emit('newMessage',generateMessage(user.name,message.text));
+
+}
+    
     callback();
         // from:message.from,
         // text:message.text,
@@ -71,7 +77,10 @@ socket.on('createMessage',(message,callback)=>{
 });
 
 socket.on('createLocationMessage',(coords)=>{
-    io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
+    var user=users.getUser(socket.id);
+    if(user){
+    io.to(user.room).emit('newLocationMessage',generateLocationMessage(user.name,coords.latitude,coords.longitude));
+    }
 });
 
 
